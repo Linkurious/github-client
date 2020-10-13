6313f6b0c81d00f5497251093df7f4e61ca67e72
Alexander Milevski <info@w8r.name>
2020-10-13 20:09:30 +0200

    Checking webhooks

--- test/index.test.js
+++ test/index.test.js
@@ -36,6 +36,7 @@ describe('Github client', () => {
   it('should be able to update a file', () => {
     const key = getEnvVariable('API_KEY');
     console.log(key);
+    console.log(key);
   });
 });
 
