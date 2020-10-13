810ab033123f8c1afe7177507fd82d0ba05c6fb6
Alexander Milevski <info@w8r.name>
2020-10-13 20:20:08 +0200

    Trying the CI

--- test/index.test.js
+++ test/index.test.js
@@ -35,7 +35,7 @@ describe('Github client', () => {
 
   it('should be able to update a file', () => {
     const key = getEnvVariable('API_KEY');
-    console.log(key);
+    console.log('put out', key);
   });
 });
 
